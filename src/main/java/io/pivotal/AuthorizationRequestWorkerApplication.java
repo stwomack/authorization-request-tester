8f76ffecb000543780f854aea9bde1336e842048
Steve Womack <stwomack@gmail.com>
2016-03-25 20:40:35 -0500

    Works

--- src/main/java/io/pivotal/AuthorizationRequestWorkerApplication.java
+++ src/main/java/io/pivotal/AuthorizationRequestWorkerApplication.java
@@ -1,9 +1,7 @@
-package pivotal.io;
+package io.pivotal;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import pivotal.io.service.OutputProcessorTester;
 
 import java.io.IOException;
 
--- src/main/java/io/pivotal/service/OutputProcessorTester.java
+++ src/main/java/io/pivotal/service/OutputProcessorTester.java
@@ -1,4 +1,4 @@
-package pivotal.io.service;
+package io.pivotal.service;
 
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.cloud.cloudfoundry.com.fasterxml.jackson.databind.ObjectMapper;
@@ -9,8 +9,7 @@ import org.springframework.integration.annotation.InboundChannelAdapter;
 import org.springframework.integration.annotation.Poller;
 import org.springframework.integration.core.MessageSource;
 import org.springframework.messaging.support.GenericMessage;
-import org.springframework.messaging.support.MessageBuilder;
-import pivotal.io.domain.AuthorizationRequest;
+import io.pivotal.domain.AuthorizationRequest;
 
 import java.io.IOException;
 import java.util.Date;

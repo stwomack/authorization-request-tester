c6083adaaaa0f691ceb64ca59612e322f0a52a1b
Steve Womack <stwomack@gmail.com>
2016-03-28 11:07:21 -0500

    Changes cardNumber to String, adds list of cc numbers

--- src/main/java/io/pivotal/service/OutputProcessorTester.java
+++ src/main/java/io/pivotal/service/OutputProcessorTester.java
@@ -12,7 +12,9 @@ import org.springframework.integration.core.MessageSource;
 import org.springframework.messaging.support.GenericMessage;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Random;
 
 @EnableBinding(Source.class)
@@ -28,7 +30,7 @@ public class OutputProcessorTester {
 
     public AuthorizationRequest createTestPayload() {
         AuthorizationRequest authorizationRequest = new AuthorizationRequest(
-                randLong(), randLong(), randLong(),
+                randLong(), randLong(), getRandomCardNumber(),
                 Math.abs(new Random().nextDouble()), RandomStringUtils.randomAlphabetic(8), new Date());
         return authorizationRequest;
     }
@@ -44,8 +46,16 @@ public class OutputProcessorTester {
         return (new Random().nextLong() % (max - min)) + min;
     }
 
-    private Long getRandomCardNumber() {
-//        new Random().
-
+    private String getRandomCardNumber() {
+        List<String> cardList = new ArrayList<String>();
+        cardList.add("5105 1051 0510 5100");
+        cardList.add("5185 5408 1000 0019");
+        cardList.add("5204 2300 8000 0017");
+        cardList.add("5204 7400 0990 0014");
+        cardList.add("5420 9238 7872 4339");
+        cardList.add("5455 3307 6000 0018");
+        cardList.add("5506 9208 0924 3667");
+        cardList.add("5506 9215 0542 1029");
+        return cardList.get(new Random().nextInt(cardList.size()));
     }
 }
